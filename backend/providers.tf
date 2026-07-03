a3fb2371146ce7cafdbb4a4e9ba12ae8ff800ca5
andrewsharan <andrewsharan67@gmail.com>
2026-07-03 15:37:02 +0530

    Updated backend.tf

--- backend/providers.tf
+++ backend/providers.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region  = "ap-south-1"
-  profile = "Enter your SSO profile name"
+  profile = "AdministratorAccess-169340963666"
 
   default_tags {
     tags = {
